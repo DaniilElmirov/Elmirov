f98d88deccbaff41a8f2d91a70c04c3b1df1fc6b
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-02-10 02:37:58 +0700

    Добавил маппер

---
+++ app/src/main/java/com/elmirov/tinkofftesttask/data/mapper/FilmMapper.kt
@@ -0,0 +1,30 @@
+package com.elmirov.tinkofftesttask.data.mapper
+
+import com.elmirov.tinkofftesttask.data.network.model.CountryDto
+import com.elmirov.tinkofftesttask.data.network.model.FilmDto
+import com.elmirov.tinkofftesttask.data.network.model.FilmsDto
+import com.elmirov.tinkofftesttask.data.network.model.GenreDto
+import com.elmirov.tinkofftesttask.domain.entity.Film
+
+fun FilmsDto.toEntity(): List<Film> =
+    films.map {
+        it.toEntity()
+    }
+
+fun FilmDto.toEntity(): Film =
+    Film(
+        id = id,
+        name = name,
+        posterUrl = posterUrl,
+        posterPreviewUrl = posterPreviewUrl,
+        year = year,
+        description = description ?: "",
+        genres = genresDto.map { it.toGenre() },
+        countries = countriesDto.map { it.toCountry() },
+    )
+
+private fun GenreDto.toGenre(): String =
+    genre
+
+private fun CountryDto.toCountry(): String =
+    country
