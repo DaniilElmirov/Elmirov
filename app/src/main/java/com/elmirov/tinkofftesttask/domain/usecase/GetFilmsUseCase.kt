4decac6d65ae3cb016133ab4430b1cffe0a64661
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-02-10 02:33:15 +0700

    Поменял название use case

--- app/src/main/java/com/elmirov/tinkofftesttask/domain/usecase/GetFilmsUseCase.kt
+++ app/src/main/java/com/elmirov/tinkofftesttask/domain/usecase/GetFilmsUseCase.kt
@@ -4,7 +4,7 @@ import com.elmirov.tinkofftesttask.domain.entity.Film
 import com.elmirov.tinkofftesttask.domain.repository.FilmRepository
 import javax.inject.Inject
 
-class GetFilmListUseCase @Inject constructor(
+class GetFilmsUseCase @Inject constructor(
     private val repository: FilmRepository,
 ) {
     suspend operator fun invoke(): List<Film> =
