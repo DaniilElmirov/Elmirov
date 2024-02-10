5234d5d4253222637c66ea3b7d12a63a0bbb1dc5
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-02-11 03:00:54 +0700

    Добавил PagingSource

---
+++ app/src/main/java/com/elmirov/tinkofftesttask/data/network/api/KinopoiskPagingSource.kt
@@ -0,0 +1,39 @@
+package com.elmirov.tinkofftesttask.data.network.api
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.elmirov.tinkofftesttask.data.mapper.toEntity
+import com.elmirov.tinkofftesttask.domain.entity.Film
+import javax.inject.Inject
+
+class KinopoiskPagingSource @Inject constructor(
+    private val api: KinopoiskApi,
+) : PagingSource<Int, Film>() {
+
+    private companion object {
+        private const val DEFAULT_PAGE = 1
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, Film>): Int? {
+        val anchorPosition = state.anchorPosition ?: return null
+        val anchorPage = state.closestPageToPosition(anchorPosition) ?: return null
+        return anchorPage.prevKey?.plus(1) ?: anchorPage.nextKey?.minus(1)
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Film> {
+
+        val page = params.key ?: DEFAULT_PAGE
+
+        val response = api.getFilms(page)
+        val lastPage = response.lastPage
+
+        return if (response.films.isNotEmpty()) {
+            val films = response.toEntity()
+            val prevKey = if (page == DEFAULT_PAGE) null else page - 1
+            val nextKey = if (page == lastPage) null else page + 1
+            LoadResult.Page(films, prevKey, nextKey)
+        } else {
+            LoadResult.Error(Throwable())
+        }
+    }
+}
