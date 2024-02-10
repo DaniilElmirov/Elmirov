420d785fb8f2709889fbd7d399365fa6dc8a5328
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-02-11 03:01:49 +0700

    Изменил метод api

--- app/src/main/java/com/elmirov/tinkofftesttask/data/network/api/KinopoiskApi.kt
+++ app/src/main/java/com/elmirov/tinkofftesttask/data/network/api/KinopoiskApi.kt
@@ -4,6 +4,7 @@ import com.elmirov.tinkofftesttask.data.network.model.FilmDto
 import com.elmirov.tinkofftesttask.data.network.model.FilmsDto
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface KinopoiskApi {
 
@@ -12,6 +13,8 @@ interface KinopoiskApi {
         @Path("id") id: Int,
     ): FilmDto
 
-    @GET("api/v2.2/films/collections?type=TOP_250_MOVIES&page=1")
-    suspend fun getFilms(): FilmsDto
+    @GET("api/v2.2/films/collections?type=TOP_250_MOVIES")
+    suspend fun getFilms(
+        @Query("page=") page: Int,
+    ): FilmsDto
 }
