d79803e256eaaf26d7b2a4deb1b29f5d5a96c87f
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-02-11 03:00:29 +0700

    Изменил Dto

--- app/src/main/java/com/elmirov/tinkofftesttask/data/network/model/FilmsDto.kt
+++ app/src/main/java/com/elmirov/tinkofftesttask/data/network/model/FilmsDto.kt
@@ -3,5 +3,6 @@ package com.elmirov.tinkofftesttask.data.network.model
 import com.google.gson.annotations.SerializedName
 
 data class FilmsDto(
-    @SerializedName("items") val films: List<FilmDto>
+    @SerializedName("totalPages") val lastPage: Int,
+    @SerializedName("items") val films: List<FilmDto>,
 )
