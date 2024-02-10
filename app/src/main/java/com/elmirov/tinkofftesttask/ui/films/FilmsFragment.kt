3d2317b553f24afc1e625db9c60c9901b735dff7
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-02-10 15:28:10 +0700

    Добавил обработку стэйтов на экран фильмов

--- app/src/main/java/com/elmirov/tinkofftesttask/ui/films/FilmsFragment.kt
+++ app/src/main/java/com/elmirov/tinkofftesttask/ui/films/FilmsFragment.kt
@@ -2,12 +2,22 @@ package com.elmirov.tinkofftesttask.ui.films
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.elmirov.tinkofftesttask.TestTaskApplication
 import com.elmirov.tinkofftesttask.databinding.FragmentFilmsBinding
+import com.elmirov.tinkofftesttask.domain.entity.Film
+import com.elmirov.tinkofftesttask.presentation.ViewModelFactory
+import com.elmirov.tinkofftesttask.presentation.films.FilmsState
+import com.elmirov.tinkofftesttask.presentation.films.FilmsViewModel
+import com.elmirov.tinkofftesttask.ui.films.adapter.FilmAdapter
+import com.elmirov.tinkofftesttask.util.collectLifecycleFlow
+import javax.inject.Inject
 
 class FilmsFragment : Fragment() {
 
@@ -18,6 +28,13 @@ class FilmsFragment : Fragment() {
     private var _binding: FragmentFilmsBinding? = null
     private val binding
         get() = _binding!!
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+
+    private val viewModel by lazy {
+        ViewModelProvider(this, viewModelFactory)[FilmsViewModel::class.java]
+    }
 
     private val component by lazy {
         (requireActivity().application as TestTaskApplication).component
@@ -37,8 +54,62 @@ class FilmsFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setOnClickListeners()
+        applyState()
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    private fun setOnClickListeners() {
+        binding.repeat.setOnClickListener {
+            viewModel.getFilms()
+        }
+    }
+
+    private fun applyState() {
+        collectLifecycleFlow(viewModel.state) { state ->
+            when (state) {
+                FilmsState.Initial -> Unit
+                FilmsState.Loading -> showLoading()
+                is FilmsState.Content -> showContent(state.content)
+                is FilmsState.Error -> showError()
+            }
+        }
+    }
+
+    private fun showLoading() {
+        binding.films.isVisible = false
+        binding.error.isVisible = false
+        binding.progressBar.isVisible = true
+    }
+
+    private fun showContent(content: List<Film>) {
+        binding.progressBar.isVisible = false
+        binding.error.isVisible = false
+        binding.films.isVisible = true
+
+        val filmAdapter = FilmAdapter(
+            onClick = {
+                Log.d("TAGTAG", it.name)
+            },
+            onLongClickListener = {
+                Log.d("TAGTAG", "${it.name} long")
+            }
+        )
+
+        binding.films.adapter = filmAdapter
+        filmAdapter.submitList(content)
+    }
+
+    private fun showError() {
+        binding.progressBar.isVisible = false
+        binding.error.isVisible = true
+        binding.films.isVisible = false
     }
 }
