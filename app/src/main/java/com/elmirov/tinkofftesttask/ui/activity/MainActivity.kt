a97f521155cf68632b0d81b2f7181c174d5890e8
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-02-12 11:06:56 +0700

    Добавил обработку стейтов MainActivity

--- app/src/main/java/com/elmirov/tinkofftesttask/ui/activity/MainActivity.kt
+++ app/src/main/java/com/elmirov/tinkofftesttask/ui/activity/MainActivity.kt
@@ -2,15 +2,21 @@ package com.elmirov.tinkofftesttask.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.elmirov.tinkofftesttask.R
 import com.elmirov.tinkofftesttask.TestTaskApplication
 import com.elmirov.tinkofftesttask.databinding.ActivityMainBinding
 import com.elmirov.tinkofftesttask.presentation.ViewModelFactory
 import com.elmirov.tinkofftesttask.presentation.activity.MainActivityViewModel
+import com.elmirov.tinkofftesttask.presentation.activity.Screen
+import com.elmirov.tinkofftesttask.ui.films.FilmsFragment
+import com.elmirov.tinkofftesttask.ui.info.FilmInfoFragment
 import com.github.terrakok.cicerone.NavigatorHolder
 import com.github.terrakok.cicerone.Router
 import com.github.terrakok.cicerone.androidx.AppNavigator
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
@@ -43,7 +49,31 @@ class MainActivity : AppCompatActivity() {
             viewModel.openPopular()
         }
 
+        catchInfoScreen()
         setOnClickListeners()
+        applyState()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        navigatorHolder.setNavigator(navigator)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        navigatorHolder.removeNavigator()
+    }
+
+    private fun catchInfoScreen() {
+        supportFragmentManager.addOnBackStackChangedListener {
+            val currentScreen = supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+            if (currentScreen is FilmInfoFragment)
+                viewModel.infoOpened()
+
+            if (currentScreen is FilmsFragment)
+                viewModel.backToPopular()
+        }
     }
 
     private fun setOnClickListeners() {
@@ -58,13 +88,46 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        navigatorHolder.setNavigator(navigator)
+    private fun applyState() {
+        lifecycleScope.launch {
+            viewModel.state.collect {
+                when (it.currentScreen) {
+                    Screen.POPULAR -> applyPopular()
+                    Screen.FAVORITES -> applyFavorites()
+                    Screen.INFO -> applyInfo()
+                    Screen.NO_SET -> Unit
+                }
+            }
+        }
     }
 
-    override fun onPause() {
-        super.onPause()
-        navigatorHolder.removeNavigator()
+    private fun applyPopular() {
+        binding.apply {
+            popular.isEnabled = false
+            popular.isVisible = true
+
+            favorite.isEnabled = true
+            favorite.isVisible = true
+        }
+    }
+
+    private fun applyFavorites() {
+        binding.apply {
+            popular.isEnabled = true
+            popular.isVisible = true
+
+            favorite.isEnabled = false
+            favorite.isVisible = true
+        }
+    }
+
+    private fun applyInfo() {
+        binding.apply {
+            popular.isEnabled = false
+            popular.isVisible = false
+
+            favorite.isEnabled = false
+            favorite.isVisible = false
+        }
     }
 }
